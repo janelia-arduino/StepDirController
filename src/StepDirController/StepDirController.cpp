1725254c1efc01483a6df55d25baee36f9ea24f6
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-12 12:51:03 -0400

    Added const ConstantString * to handler comments.

--- src/StepDirController/StepDirController.cpp
+++ src/StepDirController/StepDirController.cpp
@@ -746,6 +746,7 @@ size_t StepDirController::channelToMotorIndex(const size_t channel)
 // const char *
 // ArduinoJson::JsonArray *
 // ArduinoJson::JsonObject *
+// const ConstantString *
 //
 // For more info read about ArduinoJson parsing https://github.com/janelia-arduino/ArduinoJson
 //
