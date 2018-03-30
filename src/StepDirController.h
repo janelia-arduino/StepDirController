2d90b25ccaf084f2e67b93fbf02d1920e3330417
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-30 14:12:21 -0400

    Removed unnecessary header includes.

--- src/StepDirController.h
+++ src/StepDirController.h
@@ -7,26 +7,14 @@
 // ----------------------------------------------------------------------------
 #ifndef STEP_DIR_CONTROLLER_H
 #define STEP_DIR_CONTROLLER_H
-#include <Flash.h>
-#include <EEPROM.h>
-#include <Streaming.h>
 #include <ArduinoJson.h>
-#include <JsonSanitizer.h>
 #include <JsonStream.h>
 #include <Array.h>
 #include <Vector.h>
-#include <MemoryFree.h>
 #include <ConstantVariable.h>
-#include <SavedVariable.h>
 #include <Functor.h>
-#include <IndexedContainer.h>
-#include <FunctorCallbacks.h>
-#include <Watchdog.h>
 
-#include <SPI.h>
 #include <TMC429.h>
-
-#include <ModularClient.h>
 
 #include <ModularServer.h>
 #include <ModularDeviceBase.h>
